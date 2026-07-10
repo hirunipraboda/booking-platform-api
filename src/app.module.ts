d1cbc33ab0f57d7e66ef8e66bb7ed6d6bd7e39a1
hirunipraboda <hirunipraboda28@gmail.com>
2026-07-11 01:26:05 +0530

    set up the project architecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ServicesModule } from './services/services.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, ServicesModule, BookingsModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
